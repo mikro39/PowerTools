5764c21654d3fa63daa305e308914e7f383539d9
Mike Romero <mikro_39@hotmail.com>
2022-01-30 11:01:24 -0700

    Days before Frost compelted - Fixed some spelling

--- GardenTools/GetPantingDate.ps1
+++ GardenTools/GetPantingDate.ps1
@@ -2,18 +2,19 @@
 
 Write-Host "Welcome to the planting Date Calculator"
 $BeforeOrAfterFrost = "Q"
-$DateOfLastFrost = 
+$FrostDate = Get-Date -Month 4 -Day 22
 
 
 while ($BeforeOrAfterFrost -ne "X") {
     Write-Host "To exit the calculator press X"
-    $BeforeOrAfterFrost = Read-Host "is the planting day before or after the first frost? (Y/N)"
+    $BeforeOrAfterFrost = Read-Host "is the planting day before the first frost? (Y/N)"
 
     if ($BeforeOrAfterFrost -eq "Y") {
         [int]$WeeksBefore = Read-Host "How many weeks before frost?"
         $WeeksBeforeToDays =  $WeeksBefore * 7
+        $BeforeResult = $FrostDate.AddDays(-$WeeksBeforeToDays)
 
-        Write-Host $WeeksBeforeToDays
+        Write-Host "The Date to Plant is "$BeforeResult
     }
     elseif ($BeforeOrAfterFrost -eq "N") {
         int $WeeksAfter = Read-Host "How many weeks after?"
